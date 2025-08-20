06ea287525612504c65d07ae881975aaf61ec01e
Lord Modiatouka <lmodiatouka@gmail.com>
2025-08-20 18:33:07 +0000

    ajoute dans le .env: apiKey: "AIzaSyBZoOXcRg7S5-cL9Wj_jl1LcXqfPQ_Qulc",

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBZoOXcRg7S5-cL9Wj_jl1LcXqfPQ_Qulc",
-  authDomain: "le-kaizen.firebaseapp.com",
-  projectId: "le-kaizen",
-  storageBucket: "le-kaizen.appspot.com",
-  messagingSenderId: "501393531134",
-  appId: "1:501393531134:web:410af19629da6746ca85af"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
