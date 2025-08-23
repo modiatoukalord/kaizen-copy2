8340bc79d8673a00770b445628f010bec7c5e150
Lord Modiatouka <lmodiatouka@gmail.com>
2025-08-23 15:19:54 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button variant="g

--- src/components/dashboard/data-table-toolbar.tsx
+++ src/components/dashboard/data-table-toolbar.tsx
@@ -10,6 +10,7 @@ import { Input } from '@/components/ui/input';
 
 import { DateRangePicker } from '../ui/date-range-picker';
 import { DateRange } from 'react-day-picker';
+import { cn } from '@/lib/utils';
 
 interface DataTableToolbarProps<TData> {
   table: Table<TData>;
@@ -33,22 +34,22 @@ export function DataTableToolbar<TData>({
   }, [date, table]);
 
   return (
-    <div className="flex items-center justify-between py-4">
-      <div className="flex flex-1 items-center space-x-2">
+    <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-2 py-4">
+      <div className="flex flex-col sm:flex-row flex-1 items-stretch sm:items-center gap-2">
         <Input
           placeholder="Filtrer..."
           value={(table.getColumn('description')?.getFilterValue() as string) ?? ''}
           onChange={(event) =>
             table.getColumn('description')?.setFilter(event.target.value)
           }
-          className="h-10 w-[120px] lg:w-[250px]"
+          className="h-10 w-full sm:w-[150px] lg:w-[250px]"
         />
         <DateRangePicker date={date} onDateChange={setDate} />
         {isFiltered && (
           <Button
             variant="ghost"
             onClick={() => table.resetColumnFilters()}
-            className="h-10 px-2 lg:px-3"
+            className="h-10 px-2 lg:px-3 justify-center"
           >
             Réinitialiser
             <X className="ml-2 h-4 w-4" />
