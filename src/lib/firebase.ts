de7fb366d386641d48ae1ae824bd0f5b4661b5ad
Lord Modiatouka <lmodiatouka@gmail.com>
2025-12-29 15:18:58 +0000

    lie l'application à cette base de donnée:  apiKey: "AIzaSyBF8uFBWV5hM47a

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
@@ -5,13 +6,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDPryWI1Z_HAfdZ8TOTiSqQql47bu7XP4E",
-  authDomain: "kheops-game.firebaseapp.com",
-  projectId: "kheops-game",
-  storageBucket: "kheops-game.appspot.com",
-  messagingSenderId: "61326874413",
-  appId: "1:61326874413:web:8fd874d77e1e45391365a4",
-  measurementId: "G-6GKV4HCBCS"
+  apiKey: "AIzaSyBF8uFBWV5hM47aSHoit5PQmvH-keDan4k",
+  authDomain: "studio-5294796188-f2001.firebaseapp.com",
+  projectId: "studio-5294796188-f2001",
+  storageBucket: "studio-5294796188-f2001.appspot.com",
+  messagingSenderId: "209995673689",
+  appId: "1:209995673689:web:9788b861af814dfd890148"
 };
 
 // Initialize Firebase
